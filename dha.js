68f3a50fa92f977304ca78394401d8b098d9397a
KurrXd ft Trash <91126186+KurrXd@users.noreply.github.com>
2021-10-14 13:39:32 +0700

    Add files via upload

--- dha.js
+++ dha.js
@@ -2682,6 +2682,7 @@ a += `\`\`\`き⃟🦈 Title : ${i.title}\`\`\`
                break
          case 'asupan': // shansekai                
                if (!isPremium) return reply(mess.only.premium)
+               if (!isOwner) return  reply(mess.only.owner)
                reply(mess.wait)
                asupan()
               .then(async (body) => {
